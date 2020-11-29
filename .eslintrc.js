6f715d7ce49559c32f5974f19e92021ff3e31df0
zico van ongeval <zico.vanongeval10@gmail.com>
2020-11-29 17:20:37 +0100

    changed axios basURL to point to amazon webservice

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,13 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
      // we only want single quotes
      'quotes': ['error', 'single'],
      // we want to force semicolons
--- src/services/ProductService.js
+++ src/services/ProductService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'http://software-security-zico.eu-central-1.elasticbeanstalk.com/',
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
